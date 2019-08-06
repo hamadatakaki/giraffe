846fa49aa7d3f00d361ecf0b033f2c162fdfe9dd
hamadatakaki <i16hamadat@gmail.com>
2019-08-06 16:15:48 +0900

    Modify parsing method for index

--- src/objects/index.rs
+++ src/objects/index.rs
@@ -1,4 +1,4 @@
-use crate::utils::read_file_all;
+use crate::utils::{read_file_all, parse_from_vec_u8};
 use super::entry::Entry;
 
 use byteorder::{BigEndian, ReadBytesExt};
@@ -24,52 +24,65 @@ impl Index {
         }
     }
 
-    // TODO: 生成過程のテストを書く
     pub fn make_object_from_path(path: &str) -> Result<Self, Box<std::error::Error>> {
         let buf = read_file_all(path)?;
         let (identifer, header_and_body) = buf.split_at(4);
 
         assert!(is_index(identifer.to_vec()));
 
-        let (header, _body) = header_and_body.split_at(8);
+        let (header, mut _body) = header_and_body.split_at(8);
         let (version, entry_number) = Self::analyze_header(header)?;
         let mut entries: Vec<Entry> = Vec::new();
 
         let mut c: u32 = 0;
 
         loop {
-            let (mut ctime_sec, _body) = _body.split_at(4);
-            let (mut ctime_nano, _body) = _body.split_at(4);
-            let (mut mtime_sec, _body) = _body.split_at(4);
-            let (mut mtime_nano, _body) = _body.split_at(4);
-            let (mut dev, _body) = _body.split_at(4);
-            let (mut inode, _body) = _body.split_at(4);
-            let (mut mode, _body) = _body.split_at(4);
-            let (mut uid, _body) = _body.split_at(4);
-            let (mut guid, _body) = _body.split_at(4);
-            let (mut size, _body) = _body.split_at(4);
-            let (sha1, _body) = _body.split_at(20);
+            let (mut ctime_sec, body) = _body.split_at(4);
+            let (mut ctime_nano, body) = body.split_at(4);
+            let (mut mtime_sec, body) = body.split_at(4);
+            let (mut mtime_nano, body) = body.split_at(4);
+            let (mut dev, body) = body.split_at(4);
+            let (mut inode, body) = body.split_at(4);
+            let (mut mode, body) = body.split_at(4);
+            let (mut uid, body) = body.split_at(4);
+            let (mut guid, body) = body.split_at(4);
+            let (mut size, body) = body.split_at(4);
+            let (sha1, body) = body.split_at(20);
 
             let ctime_sec = ctime_sec.read_u32::<BigEndian>()? as i64;
+            println!("{:?}", ctime_sec);
             let ctime_nano = ctime_nano.read_u32::<BigEndian>()?;
+            println!("{:?}", ctime_nano);
             let mtime_sec = mtime_sec.read_u32::<BigEndian>()? as i64;
+            println!("{:?}", mtime_sec);
             let mtime_nano = mtime_nano.read_u32::<BigEndian>()?;
+            println!("{:?}", mtime_nano);
             let dev = dev.read_u32::<BigEndian>()?;
+            println!("dev{:?}", dev);
             let mode_u32 = mode.read_u32::<BigEndian>()?;
             let mode = format!("{:o}", mode_u32);
+            println!("mode{:?}", mode);
             let inode = inode.read_u32::<BigEndian>()?;
+            println!("{:?}", inode);
             let uid = uid.read_u32::<BigEndian>()?;
+            println!("uid{:?}", uid);
             let guid = guid.read_u32::<BigEndian>()?;
+            println!("{:?}", guid);
             let size = size.read_u32::<BigEndian>()?;
-            let sha1 = sha1.iter().map(|x| format!("{:o}", x)).collect::<String>();
+            println!("{:?}", size);
+            let sha1 = sha1.iter().map(|x| format!("{:x}", x)).collect::<String>();
+            println!("sha1 {:?}", sha1);
 
-            let (mut name_length, _body) = _body.split_at(2);
-            let name_length = name_length.read_u32::<BigEndian>()? as usize;
-            let (name, _body) = _body.split_at(name_length);
+            let (name_length, body) = body.split_at(2);
+            println!("hoge");
+            let name_length = parse_from_vec_u8(name_length) as usize;
+            println!("len {:?}", name_length);
+            let (name, body) = body.split_at(name_length);
             let name = std::str::from_utf8(name)?.to_string();
-            let (_padding, _body) = _body.split_at(8-(6+name_length)%8);
-
-            println!("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}", ctime_sec, ctime_nano, mtime_sec, mtime_nano, dev, inode, mode, uid, guid, size, sha1, name, (8-(6+name_length)%8));
+            println!("name {:?}", name);
+            let (_padding, body) = body.split_at(8-(6+name_length)%8);
+            println!("pad {:?}", _padding);
+            _body = body;
 
             let entry = Entry::new(ctime_sec, ctime_nano, mtime_sec, mtime_nano, dev, inode, mode, uid, guid, size, sha1, name);
             entries.push(entry);
