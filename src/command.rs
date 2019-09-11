30b091293de7e40d067b8463ac9b17f299b38137
hamadatakaki <i16hamadat@gmail.com>
2019-09-11 23:44:10 +0900

    Modify path

--- src/command.rs
+++ src/command.rs
@@ -2,16 +2,16 @@ use std::fs::{create_dir_all, write};
 use std::path::Path;
 
 pub fn init() -> std::io::Result<i32> {
-    let repo_path = Path::new("./.repo");
+    let repo_path = Path::new(".repo");
 
     if repo_path.exists() {
         print!("already initialized\n");
         Ok(1)
     } else {
-        create_dir_all("./.repo/objects")?;
-        create_dir_all("./.repo/refs/heads")?;
-        write("./.repo/HEAD", "refs/heads/master\n")?;
-        write("./.repo/refs/heads/master", "")?;
+        create_dir_all(".repo/objects")?;
+        create_dir_all(".repo/refs/heads")?;
+        write(".repo/HEAD", "refs/heads/master\n")?;
+        write(".repo/refs/heads/master", "")?;
         Ok(0)
     }
 }
